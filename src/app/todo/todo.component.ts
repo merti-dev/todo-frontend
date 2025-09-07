8b8267fcaf4e15536a013cf9b82fecdb95f21915
mert <mert.inal@gmail.com>
2025-09-07 12:32:35 +0200

    todo last

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
